787a2f5806db6bc1d67a882cd66664bdc6a60d07
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-09-02 11:39:44 +1200

    Get cluster types from db

--- source/pages/Create2.php
+++ source/pages/Create2.php
@@ -5,7 +5,8 @@ session_start();
 $_SESSION['test_date'] = $_POST['test_date'];
 $_SESSION['test_name'] = $_POST['test_name'];
 $_SESSION['test_room'] = $_POST['test_room'];
-$_SESSION['test_time'] = $_POST['test_time'];
+$_SESSION['test_stime'] = $_POST['test_stime'];
+$_SESSION['test_etime'] = $_POST['test_etime'];
 
 echo $_SESSION['test_date'];
 ?>
@@ -22,31 +23,82 @@ echo $_SESSION['test_date'];
         <br > <br > <br >
 
         <form name="Type" action="Create3.php" method="POST">
-            <table>
+            <table id="clusters">
                 <tr>
                     <th> Select </th>
                     <th> Type </th>
                     <th> Description </th>
-                </tr>
-                <tr>
-                    <td> <input type="radio" name="test_type" value="type1" /> </td>
-                    <td> 1st test type </td>
-                    <td> 1st test type description </td>
-                </tr>
-                <tr>
-                    <td> <input type="radio" name="test_type" value="type2" /> </td>
-                    <td> 2nd test type </td>
-                    <td> 2nd test type description </td>
-                </tr>
-                <tr>
-                    <td> <input type="radio" name="test_type" value="type3" /> </td>
-                    <td> 3rd test type </td>
-                    <td> Add in php from db </td>
                 </tr>
             </table>
 
             <input type="submit" value="Next" />
         </form>
 
+        <script>
+            nocache = "&nocache=1";
+            request = new asyncRequest()
+            request.open("GET", "Create_Helper.php?item=Clusters" + nocache, true)
+            request.onreadystatechange = function()
+            {
+                if (this.readyState == 4)
+                {
+                    if (this.status == 200)
+                    {
+                        if (this.responseText != null)
+                        {
+                            let table = document.getElementById('clusters');
+                            console.log(this.responseText);
+                            // NEED TO CATCH ERROR IF PARSE FAILS
+                            let clusters = JSON.parse(this.responseText);
+                            for (let i=0; i<clusters.length; i++) {
+                                let row = table.insertRow(i+1);
+                                let idCell = row.insertCell(0);
+                                let nameCell = row.insertCell(1);
+                                let descripCell = row.insertCell(2);
+
+                                let radioBut = document.createElement('input');
+                                radioBut.type = "radio";
+                                radioBut.name = "test_type";
+                                radioBut.value = clusters[i][0];
+                                idCell.appendChild(radioBut);
+
+                                nameCell.innerHTML = clusters[i][1];
+                                descripCell.innerHTML = clusters[i][2];
+                            }
+                        }
+                        else alert("Communication error: No data received")
+                    }
+                    else alert( "Communication error: " + this.statusText)
+                }
+            }
+            request.send(null)
+            function asyncRequest()
+            {
+                let request;
+                try
+                {
+                    request = new XMLHttpRequest();
+                }
+                catch(e1)
+                {
+                    try
+                    {
+                        request = new ActiveXObject("Msxml2.XMLHTTP");
+                    }
+                    catch(e2)
+                    {
+                        try
+                        {
+                            request = new ActiveXObject("Microsoft.XMLHTTP");
+                        }
+                        catch(e3)
+                        {
+                            request = false
+                        }
+                    }
+                }
+                return request
+            }
+        </script>
     </body>
 </html>
