0e15c81bc317a36190e9e90fe115f7940bffff0d
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-09-08 21:08:01 +1200

    Create CSS and JS file for the nav bar

---
+++ source/js/NavBar.js
@@ -0,0 +1,11 @@
+// **** NavBar Stuff ****
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "175px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
