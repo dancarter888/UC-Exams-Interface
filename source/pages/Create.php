6fb736f868b23e555ca137e439f8573db0fa9c91
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-09-08 20:12:52 +1200

    Recommit stuff that got lost

--- source/pages/Create.php
+++ source/pages/Create.php
@@ -107,10 +107,11 @@
         Test Date <input type="date" name="test_date" /> <br />
         Test Name <input type="text" name="test_name" required /> <br />
         Room
-        <input list="rooms" name="test_room" multiple>
+        <div id="room_select"></div>
+        <input list="rooms" id="test_room" name="test_room">
         <datalist id="rooms" required>
             <option value="" disabled selected> -- Select a room -- </option>
-        </datalist> <br />
+        </datalist> <button onclick="addRoom()">Add</button><br />
         Start Time <input type="time" name="test_stime" required /> <br />
         End Time <input type="time" name="test_etime" required /> <br />
 
@@ -120,6 +121,9 @@
     <h4 id="info"></h4>
 
     <script>
+        let ROOMS = [];
+        let roomsAdded = 0;
+
         nocache = "&nocache=1";
         request = new asyncRequest()
         request.open("GET", "Create_Helper.php?item=Rooms" + nocache, true)
@@ -134,6 +138,7 @@
                         let selectElement = document.getElementById('rooms');
                         console.log(this.responseText);
                         let rooms = JSON.parse(this.responseText);
+                        ROOMS = rooms;
                         for (let i=0; i<rooms.length; i++) {
                             let option = document.createElement('option');
                             option.value = rooms[i];
@@ -147,6 +152,7 @@
             }
         }
         request.send(null)
+
         function asyncRequest()
         {
             let request;
@@ -173,6 +179,35 @@
                 }
             }
             return request
+        }
+
+        /**
+         * If the input from datalist is valid then add it to the selected rooms.
+         */
+        function addRoom()
+        {
+            let roomDiv = document.getElementById("room_select");
+            let input = document.getElementById("test_room");
+            if (ROOMS.includes(input.value)) {
+                // Create a new input tag for the selected room
+                let item = document.createElement("input");
+                item.name = `room${roomsAdded++}`;
+                item.value = input.value;
+                item.readOnly = true;
+                roomDiv.appendChild(item);
+                roomDiv.appendChild(document.createElement("br"));
+
+                // Remove option for this room so it can't be selected more than once
+                let dList = document.getElementById("rooms");
+                for (let i=0; i<dList.children.length; i++) {
+                    if (dList.children[i].value === input.value) {
+                        dList.children[i].remove();
+                        break;
+                    }
+                }
+                ROOMS.splice(ROOMS.indexOf(input.value), 1);
+                input.value = "";
+            }
         }
     </script>
 
