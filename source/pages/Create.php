19c08310cbe2251bc515728541af2cb1658cc9c8
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-09-08 21:08:46 +1200

    Tidy up create files

--- source/pages/Create.php
+++ source/pages/Create.php
@@ -1,217 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="../css/login.css">
-    <title>Create</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <head>
+        <!-- CSS -->
+        <link rel="stylesheet" href="../css/login.css">
+        <link rel="stylesheet" href="../css/NavBar.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <style>
-        body {
-            font-family: "Lato", sans-serif;
-        }
+        <!-- JavaScript -->
+        <script src="../js/AJAX.js"></script>
+        <script src="../js/NavBar.js"></script>
 
-        .sidebar {
-            height: 100%;
-            width: 0;
-            position: fixed;
-            z-index: 1;
-            top: 0;
-            left: 0;
-            background-color: #111;
-            overflow-x: hidden;
-            transition: 0.5s;
-            padding-top: 60px;
-        }
+        <title>Create</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    </head>
+    <body>
+        <div id="mySidebar" class="sidebar">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+            <a href="Create.php">Create Event</a>
+            <br> <br> <br>
+            <a href="login.php">Logout</a>
+        </div>
 
-        .sidebar a {
-            padding: 20px 10px 10px 32px;
-            text-decoration: none;
-            font-size: 20px;
-            color: grey;
-            display: block;
-            transition: 0.3s;
-        }
+        <div id="main">
+            <button class="openbtn" onclick="openNav()"> ☰ </button>
+        </div>
 
-        .sidebar a:hover {
-            color: white;
-        }
+        <h1> Create Test Step 1 </h1>
 
+        <br > <br > <br >
 
-        .sidebar .closebtn {
-            position: absolute;
-            top: 0;
-            right: 25px;
-            font-size: 36px;
-            margin-left: 50px;
-        }
+        <form name="Details" action="Create2.php" method="post">
+            Test Date <input type="date" name="test_date" /> <br />
+            Test Name <input type="text" name="test_name" required /> <br />
+            Room
+            <div id="room_select"></div>
+            <input list="rooms" id="test_room" name="test_room">
+            <datalist id="rooms" required>
+                <option value="" disabled selected> -- Select a room -- </option>
+            </datalist> <button onclick="addRoom()">Add</button><br />
+            Start Time <input type="time" name="test_stime" required /> <br />
+            End Time <input type="time" name="test_etime" required /> <br />
 
-        .openbtn {
-            font-size: 20px;
-            cursor: pointer;
-            background-color: #111;
-            color: white;
-            padding: 10px 15px;
-            border: none;
-        }
+            <input type="submit" value="Next" />
+        </form>
 
+        <h4 id="info"></h4>
 
+        <script>
+            let ROOMS = [];
+            let roomsAdded = 0;
 
-        .openbtn:hover {
-            background-color: #444;
-        }
+            // Make a get request to the URL
+            makeRequest("GET", "Create_Helper.php?item=Rooms", roomCallback);
 
-        #main {
-            transition: margin-left .5s;
-            padding: 5px;
-        }
+            /**
+             * Function to add the rooms in response text to the datalist in the webpage.
+             **/
+            function roomCallback(responseText) {
+                let selectElement = document.getElementById('rooms');
+                console.log(responseText);
+                let rooms = JSON.parse(responseText);
+                ROOMS = rooms;
+                for (let i=0; i<rooms.length; i++) {
+                    let option = document.createElement('option');
+                    option.value = rooms[i];
+                    option.innerHTML = rooms[i];
+                    selectElement.appendChild(option);
+                }
+            }
 
-        /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-        @media screen and (max-height: 450px) {
-            .sidebar {padding-top: 15px;}
-            .sidebar a {font-size: 18px;}
-        }
-    </style>
-
-</head>
-<body>
-<div id="mySidebar" class="sidebar">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-    <a href="Create.php">Create Event</a>
-    <br> <br> <br>
-    <a href="login.php">Logout</a>
-</div>
-
-<div id="main">
-    <button class="openbtn" onclick="openNav()"> ☰ </button>
-</div>
-
-<script>
-    function openNav() {
-        document.getElementById("mySidebar").style.width = "175px";
-        document.getElementById("main").style.marginLeft = "250px";
-    }
-
-    function closeNav() {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
-    }
-
-</script>
-
-<h1> Create Test Step 1 </h1>
-
-    <br > <br > <br >
-
-    <form name="Details" action="Create2.php" method="post">
-        Test Date <input type="date" name="test_date" /> <br />
-        Test Name <input type="text" name="test_name" required /> <br />
-        Room
-        <div id="room_select"></div>
-        <input list="rooms" id="test_room" name="test_room">
-        <datalist id="rooms" required>
-            <option value="" disabled selected> -- Select a room -- </option>
-        </datalist> <button onclick="addRoom()">Add</button><br />
-        Start Time <input type="time" name="test_stime" required /> <br />
-        End Time <input type="time" name="test_etime" required /> <br />
-
-        <input type="submit" value="Next" />
-    </form>
-
-    <h4 id="info"></h4>
-
-    <script>
-        let ROOMS = [];
-        let roomsAdded = 0;
-
-        nocache = "&nocache=1";
-        request = new asyncRequest()
-        request.open("GET", "Create_Helper.php?item=Rooms" + nocache, true)
-        request.onreadystatechange = function()
-        {
-            if (this.readyState == 4)
+            /**
+             * If the input from datalist is valid then add it to the selected rooms.
+             */
+            function addRoom()
             {
-                if (this.status == 200)
-                {
-                    if (this.responseText != null)
-                    {
-                        let selectElement = document.getElementById('rooms');
-                        console.log(this.responseText);
-                        let rooms = JSON.parse(this.responseText);
-                        ROOMS = rooms;
-                        for (let i=0; i<rooms.length; i++) {
-                            let option = document.createElement('option');
-                            option.value = rooms[i];
-                            option.innerHTML = rooms[i];
-                            selectElement.appendChild(option);
+                let roomDiv = document.getElementById("room_select");
+                let input = document.getElementById("test_room");
+                if (ROOMS.includes(input.value)) {
+                    // Create a new input tag for the selected room
+                    let item = document.createElement("input");
+                    item.name = `room${roomsAdded++}`;
+                    item.value = input.value;
+                    item.readOnly = true;
+                    roomDiv.appendChild(item);
+                    roomDiv.appendChild(document.createElement("br"));
+
+                    // Remove option for this room so it can't be selected more than once
+                    let dList = document.getElementById("rooms");
+                    for (let i=0; i<dList.children.length; i++) {
+                        if (dList.children[i].value === input.value) {
+                            dList.children[i].remove();
+                            break;
                         }
                     }
-                    else alert("Communication error: No data received")
-                }
-                else alert( "Communication error: " + this.statusText)
-            }
-        }
-        request.send(null)
-
-        function asyncRequest()
-        {
-            let request;
-            try
-            {
-                request = new XMLHttpRequest();
-            }
-            catch(e1)
-            {
-                try
-                {
-                    request = new ActiveXObject("Msxml2.XMLHTTP");
-                }
-                catch(e2)
-                {
-                    try
-                    {
-                        request = new ActiveXObject("Microsoft.XMLHTTP");
-                    }
-                    catch(e3)
-                    {
-                        request = false
-                    }
+                    ROOMS.splice(ROOMS.indexOf(input.value), 1);
+                    input.value = "";
                 }
             }
-            return request
-        }
-
-        /**
-         * If the input from datalist is valid then add it to the selected rooms.
-         */
-        function addRoom()
-        {
-            let roomDiv = document.getElementById("room_select");
-            let input = document.getElementById("test_room");
-            if (ROOMS.includes(input.value)) {
-                // Create a new input tag for the selected room
-                let item = document.createElement("input");
-                item.name = `room${roomsAdded++}`;
-                item.value = input.value;
-                item.readOnly = true;
-                roomDiv.appendChild(item);
-                roomDiv.appendChild(document.createElement("br"));
-
-                // Remove option for this room so it can't be selected more than once
-                let dList = document.getElementById("rooms");
-                for (let i=0; i<dList.children.length; i++) {
-                    if (dList.children[i].value === input.value) {
-                        dList.children[i].remove();
-                        break;
-                    }
-                }
-                ROOMS.splice(ROOMS.indexOf(input.value), 1);
-                input.value = "";
-            }
-        }
-    </script>
+        </script>
 
 
 
-</body>
+    </body>
 </html>
--- source/pages/Create2.php
+++ source/pages/Create2.php
@@ -20,101 +20,30 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- CSS -->
         <link rel="stylesheet" href="../css/login.css">
+        <link rel="stylesheet" href="../css/NavBar.css">
+
+        <!-- JavaScript -->
+        <script src="../js/AJAX.js"></script>
+        <script src="../js/NavBar.js"></script>
+
         <meta charset="UTF-8">
         <title>Create</title>
-
-        <style>
-            body {
-                font-family: "Lato", sans-serif;
-            }
-
-            .sidebar {
-                height: 100%;
-                width: 0;
-                position: fixed;
-                z-index: 1;
-                top: 0;
-                left: 0;
-                background-color: #111;
-                overflow-x: hidden;
-                transition: 0.5s;
-                padding-top: 60px;
-            }
-
-            .sidebar a {
-                padding: 20px 10px 10px 32px;
-                text-decoration: none;
-                font-size: 20px;
-                color: grey;
-                display: block;
-                transition: 0.3s;
-            }
-
-            .sidebar a:hover {
-                color: white;
-            }
-
-
-            .sidebar .closebtn {
-                position: absolute;
-                top: 0;
-                right: 25px;
-                font-size: 36px;
-                margin-left: 50px;
-            }
-
-            .openbtn {
-                font-size: 20px;
-                cursor: pointer;
-                background-color: #111;
-                color: white;
-                padding: 10px 15px;
-                border: none;
-            }
-
-
-
-            .openbtn:hover {
-                background-color: #444;
-            }
-
-            #main {
-                transition: margin-left .5s;
-                padding: 5px;
-            }
-
-            /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-            @media screen and (max-height: 450px) {
-                .sidebar {padding-top: 15px;}
-                .sidebar a {font-size: 18px;}
-            }
-        </style>
     </head>
     <body>
 
-    <div id="mySidebar" class="sidebar">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-        <a href="Create.php">Create Event</a>
-        <br> <br> <br>
-        <a href="login.php">Logout</a>
-    </div>
+        <div id="mySidebar" class="sidebar">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+            <a href="Create.php">Create Event</a>
+            <br> <br> <br>
+            <a href="login.php">Logout</a>
+        </div>
 
-    <div id="main">
-        <button class="openbtn" onclick="openNav()"> ☰ </button>
-    </div>
+        <div id="main">
+            <button class="openbtn" onclick="openNav()"> ☰ </button>
+        </div>
 
-    <script>
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "175px";
-            document.getElementById("main").style.marginLeft = "200px";
-        }
-
-        function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft= "0";
-        }
-    </script>
         <h1> Create Test Step 2 </h1>
 
         <br > <br > <br >
@@ -132,69 +61,28 @@
         </form>
 
         <script>
-            nocache = "&nocache=1";
-            request = new asyncRequest()
-            request.open("GET", "Create_Helper.php?item=Clusters" + nocache, true)
-            request.onreadystatechange = function()
-            {
-                if (this.readyState == 4)
-                {
-                    if (this.status == 200)
-                    {
-                        if (this.responseText != null)
-                        {
-                            let table = document.getElementById('clusters');
-                            console.log(this.responseText);
-                            // NEED TO CATCH ERROR IF PARSE FAILS
-                            let clusters = JSON.parse(this.responseText);
-                            for (let i=0; i<clusters.length; i++) {
-                                let row = table.insertRow(i+1);
-                                let idCell = row.insertCell(0);
-                                let nameCell = row.insertCell(1);
-                                let descripCell = row.insertCell(2);
+            makeRequest("GET", "Create_Helper.php?item=Clusters", clusterCallback);
 
-                                let radioBut = document.createElement('input');
-                                radioBut.type = "radio";
-                                radioBut.name = "test_type";
-                                radioBut.value = clusters[i][1];
-                                idCell.appendChild(radioBut);
+            function clusterCallback(responseText) {
+                let table = document.getElementById('clusters');
+                console.log(responseText);
+                // NEED TO CATCH ERROR IF PARSE FAILS
+                let clusters = JSON.parse(responseText);
+                for (let i = 0; i < clusters.length; i++) {
+                    let row = table.insertRow(i + 1);
+                    let idCell = row.insertCell(0);
+                    let nameCell = row.insertCell(1);
+                    let descripCell = row.insertCell(2);
 
-                                nameCell.innerHTML = clusters[i][1];
-                                descripCell.innerHTML = clusters[i][2];
-                            }
-                        }
-                        else alert("Communication error: No data received")
-                    }
-                    else alert( "Communication error: " + this.statusText)
+                    let radioBut = document.createElement('input');
+                    radioBut.type = "radio";
+                    radioBut.name = "test_type";
+                    radioBut.value = clusters[i][1];
+                    idCell.appendChild(radioBut);
+
+                    nameCell.innerHTML = clusters[i][1];
+                    descripCell.innerHTML = clusters[i][2];
                 }
-            }
-            request.send(null)
-            function asyncRequest()
-            {
-                let request;
-                try
-                {
-                    request = new XMLHttpRequest();
-                }
-                catch(e1)
-                {
-                    try
-                    {
-                        request = new ActiveXObject("Msxml2.XMLHTTP");
-                    }
-                    catch(e2)
-                    {
-                        try
-                        {
-                            request = new ActiveXObject("Microsoft.XMLHTTP");
-                        }
-                        catch(e3)
-                        {
-                            request = false
-                        }
-                    }
-                }
-                return request
             }
         </script>
     </body>
