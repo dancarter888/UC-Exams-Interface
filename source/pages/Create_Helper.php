e3824cd30886b1c9d9f03f9d3f162e45c2600bff
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-09-29 15:47:46 +1300

    Start DB stuff

--- source/pages/Create_Helper.php
+++ source/pages/Create_Helper.php
@@ -7,11 +7,9 @@
     {
         if ($_GET['item'] === $ITEMS[0]) {
             $rooms = getRooms();
-            //$rooms = array('Erskine', 'Library');
             echo json_encode($rooms);
         } else if ($_GET['item'] === $ITEMS[1]) {
             $clusters = getClusters();
-            //$clusters = array(array(2, "UC-Learn", "Skills testing autologin"), array(3, "Labs", "Math Dept student undergrad labs"));
             echo json_encode($clusters);
         }
     }
@@ -24,12 +22,33 @@
         $event = $_POST["user"];
         // Associative array of the JSON
         $decoded = json_decode($event, true);
-        echo json_encode(array_keys($decoded));
+        //echo json_encode($decoded["Name"]);
 
         //Create Query
+        $qName = $decoded["Name"];
+        $qRooms = "";
+        foreach ($decoded["Rooms"] as $room) {
+            //array_push($qRooms, $room);
+            $qRooms = $qRooms . "{$room}";
+            if ($room != end($decoded["Rooms"])) {
+                $qRooms = $qRooms . ",";
+            }
+        }
+        $cluster = $decoded["TestType"];
+        $startTime = $decoded["StartTime"];
+        $endTime = $decoded["EndTime"];
+        $duration = strtotime(strtotime($endTime) - strtotime($startTime));
+        $duration = "01:00:00";
+
+        $qDate = $decoded["Date"];
+        $qDay = 1;
+        $qWeek = 34;
+        $qYear = 2020;
 
         //Call stored procedure
-
+        //$result = queryDB("CALL add_event('{$qName}','{$qRooms}','{$cluster}',{$qDay},{$qWeek},{$qYear},'{$startTime}','{$duration}')");
+        $result = queryDB('call add_event("EMTH119-20S2 Tuesday", "Erskine-033,Erskine-035,Erskine-036,Erskine-038", "MapleTA", 2, 34, 2020, "18:00:00", "01:00:00");');
+        echo json_encode($result);
     }
 
     /**
