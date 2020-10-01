d12f3a6a66af29250d9aca426a450bbe0d6e24e2
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-10-01 16:40:53 +1300

    Add add_action procedure

--- tserver.sql
+++ tserver.sql
@@ -826,6 +826,32 @@ DELIMITER ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
+/*!50003 DROP PROCEDURE IF EXISTS `add_event` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_action`(
+IN event_id int(11),
+IN time_offset time,
+IN cluster_id int(11),
+IN activate int(11)
+)
+BEGIN
+
+insert into front_action (event_id, time_offset, cluster_id, activate) values (event_id, time_offset, cluster_id, activate);
+
+END
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
 --
 -- Final view structure for view `vw_front_event`
 --
