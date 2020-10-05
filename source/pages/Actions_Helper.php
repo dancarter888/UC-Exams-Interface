05bd83c2c50c00fe8e103716d3a7c3581c3ca326
jod25 <jod25@uclive.ac.nz>
2020-10-06 12:37:31 +1300

    Changed Actions_Helper.php.php so it uses input from the config file to connect to the database

--- source/pages/Actions_Helper.php
+++ source/pages/Actions_Helper.php
@@ -1,33 +1,20 @@
 <?php
 require_once("../config/config.php");
 
-//if (isset($_GET['start']) && isset($_GET['end']))
-//{
-//    $start_date = $_GET['start'];
-//    if ($start_date === "today") {
-//        $start_date = date("Y-m-d");
-//    }
-//    $end_date = $_GET['end'];
-//    if (strtotime($start_date) !== false && strtotime($end_date) !== false) {
-//        $get_actions = getActions($start_date, $end_date);
-//        $field_names = $get_actions[0];
-//        $actions = $get_actions[1];
-//        echo json_encode([$field_names, $actions]);
-//    }
-//}
+$conn = new mysqli($hostname, $username, $password, $database);
 
 if (isset($_GET['event_id']) && isset($_GET['cluster_id']))
 {
     $event_id = $_GET['event_id'];
     $cluster_id= $_GET['cluster_id'];
-    $get_actions = getActions($event_id, $cluster_id);
+    $get_actions = getActions($conn, $event_id, $cluster_id);
     $field_names = $get_actions[0];
     $actions = $get_actions[1];
     echo json_encode([$field_names, $actions]);
 }
 
-function getActions($start_date, $end_date) {
-    $result = queryDB($start_date, $end_date);
+function getActions($conn, $start_date, $end_date) {
+    $result = queryDB($conn, $start_date, $end_date);
     $field_names = [];
     $rows = [];
     while ($field = $result->fetch_field()) {
@@ -53,13 +40,7 @@ function getSearchedActions($search) {
     return [$field_names, $rows];
 }
 
-function queryDB($start_date, $end_date) {
-    $hostname = "127.0.0.1";
-    $database = "tserver";
-    $username = "root";
-    $password = "mysql";
-    $conn = new mysqli($hostname, $username, $password, $database);
-
+function queryDB($conn, $start_date, $end_date) {
     $query = "CALL show_actions(?, ?)";
     $stmt = $conn->prepare($query);
     $stmt->bind_param('ss', $start_date, $end_date);
@@ -79,4 +60,6 @@ function sanitizeString($var) {
     $var = htmlentities($var);
     return $var;
 }
+
+$conn->close();
 ?>
