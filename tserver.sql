b02e9b17767d2aed5695042f39fc24eb6c1c3fd0
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-10-14 15:28:18 +1300

    add get start time procedure

--- tserver.sql
+++ tserver.sql
@@ -975,6 +975,33 @@ DELIMITER ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
+/*!50003 DROP PROCEDURE IF EXISTS `get_event_start_time` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_modeshow_actions       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE PROCEDURE `get_event_start_time`(
+IN event_id_in int(11)
+)
+BEGIN
+	/* Get the action using the event id */
+    select distinct start_time `time`
+    from ((`front_action` `a` join `front_cluster` on `a`.`cluster_id` = `front_cluster`.`cluster_id`) join `front_daily` on `a`.`event_id` = `front_daily`.`event_id`)
+    where a.event_id = event_id_in;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+
+
 
 
 --
