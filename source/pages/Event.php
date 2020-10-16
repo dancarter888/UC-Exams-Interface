c3892d4868fe72e4c21fe1834e97e2a347c27557
Daniel Siemers <daniel.siemers@gmail.com>
2020-10-16 20:53:48 +1300

    Reverted back

--- source/pages/Event.php
+++ source/pages/Event.php
@@ -242,8 +242,8 @@ if (!isset($_COOKIE['loggedin'])) {
     }
 
     /**
-    * Modifies the DOM to create a modal where one can edit the actions of an event.
-    */
+     * Modifies the DOM to create a modal where one can edit the actions of an event.
+     */
     function fillEditModal() {
         $("#edit-body tr").remove();
         $("#edit-body div").remove();
@@ -256,7 +256,7 @@ if (!isset($_COOKIE['loggedin'])) {
                 </div>`);
         $("#edit-body").append('<tr> <th> Action id </th> <th> Cluster Name </th> <th> Time </th> <th> Activation </th> </tr>');
         let editTable = document.getElementById('edit-body');
-        for (let i = 0; i < actions.length; i++) {
+        for (let i = 1; i < actions.length; i++) {
             let tableRow = document.createElement('tr');
             tableRow.className = 'action';
             for (let j = 0; j < actions[i].length; j++) {
@@ -297,24 +297,26 @@ if (!isset($_COOKIE['loggedin'])) {
                 </div>`);
         $("#edit-body").append(`
                 <div>
-                    <input class="form-control" list="clusters_list" placeholder="-- Select a Cluster --" id="action_cluster" required>
-                    <datalist id="clusters_list" required></datalist>
-                    <label for="OffsetInput">Action time</label>
-                    <input class="form-control" id="OffsetInput" type="time" required>
-                    <label for="Activate">Activation</label>
-                    <select class="form-control" id="Activate" required>
-                        <option value="1">Turn ON</option>
-                        <option value="0">Turn OFF</option>
-                    </select>
-                    <br/>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-4"></div>
-                            <div class="col-3">
-                                <button onclick="addAction()" id="AddAction" value="Add Action" class="btn btn-success"/> Add Action </button>
+                    <form id="ActionsForm">
+                        <input class="form-control" list="clusters_list" placeholder="-- Select a Cluster --" id="action_cluster" required>
+                        <datalist id="clusters_list" required></datalist>
+                        <label for="OffsetInput">Action time</label>
+                        <input class="form-control" id="OffsetInput" type="time" required>
+                        <label for="Activate">Activation</label>
+                        <select class="form-control" id="Activate" required>
+                            <option value="1">Turn ON</option>
+                            <option value="0">Turn OFF</option>
+                        </select>
+                        <br/>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-3">
+                                    <input type="submit" id="AddAction" value="Add Action" class="btn btn-success"/>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>`);
         makeRequest("GET", "Create_Helper.php?item=Clusters", clusterCallback);
 
@@ -324,9 +326,7 @@ if (!isset($_COOKIE['loggedin'])) {
     //This ensures that the fields in the form are required
     $('body').on("submit", "#ActionsForm", function() {
         addAction();
-        document.location.href = `Event.php?event_id=${event_id}&date=${date}`;
     });
-
 
     /**
      * Retrieves values from the action form within the add action modal.
@@ -349,7 +349,7 @@ if (!isset($_COOKIE['loggedin'])) {
             url: "Create_Helper.php",
             type: "post",
             data: {"action": jsonStr},
-            success: function(result) { alert("Action added"); document.location.href = `Event.php?event_id=${event_id}&date=${date}`; }
+            success: function(result) { alert("Action added"); location.reload();}
         });
     }
 
